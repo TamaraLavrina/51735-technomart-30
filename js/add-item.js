9de8eb207eaf090620a72d2539d1cea4119fea23
TamaraLavrina <tamara.lavrina@gmail.com>
2021-03-01 17:35:58 +0300

    1

---
+++ js/add-item.js
@@ -0,0 +1,22 @@
+const addButton = document.querySelector(".button-card-buy");
+const cartPopup = document.querySelector(".modal-catalog-cart");
+const cartClose = cartPopup.querySelector(".modal-close");
+
+addButton.addEventListener("click", function () {
+  evt.preventDefault();
+  cartPopup.classList.add("modal-show");
+
+});
+cartClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  cartPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (cartPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      cartPopup.classList.remove("modal-show");
+    }
+  }
+});
