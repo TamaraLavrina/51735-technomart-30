cb0aaa867848ed180c3dbde59e3bf972d6445faa
TamaraLavrina <tamara.lavrina@gmail.com>
2021-03-03 22:06:01 +0300

    мелкие правки
    внесла твои правки в css и js, отдельно поменяю vissually-hidden

--- js/add-item.js
+++ js/add-item.js
@@ -2,17 +2,20 @@
 const buyButtons = document.querySelectorAll(".button-card-buy");
 const cartPopup = document.querySelector(".modal-catalog-cart");
 const cartClose = cartPopup.querySelector(".modal-close");
+const continueClose = cartPopup.querySelector(".stay-here");
 
-buyButtons.forEach(buyButton).addEventListener("click", function() {
+buyButtons.forEach((buyButton) =>  {buyButton.addEventListener("click", function(evt){
   evt.preventDefault();
   cartPopup.classList.add("modal-show");
-      }
-    )
-
-
-;
+  })
+})
 
 cartClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  cartPopup.classList.remove("modal-show");
+});
+
+continueClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   cartPopup.classList.remove("modal-show");
 });
